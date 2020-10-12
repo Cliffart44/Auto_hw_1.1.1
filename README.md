6acdb2c1df9a6d333fb5272d5bc239ed26e08cd2
Cliffart <Cliffart44@gmail.com>
2020-10-12 19:25:08 +0300

    JUnit platform usage

---
+++ README.md
@@ -0,0 +1,1 @@
+### [Решение](https://github.com/Cliffart44/Auto_hw_1.1.1/issues/1) => [домашнего задания](https://github.com/netology-code/aqa-homeworks/tree/aqa4/basics#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0-2---junit5-%D0%B8-legacy-%D0%BD%D0%B5%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE)
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
+    /* JUnit 4 */
+
     @Test
     public void shouldProposeOne() {
         assertEquals(1, service.remain(999));
@@ -20,5 +22,22 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldProposeNull() {
         assertEquals(0, service.remain(2000));
+    }
+
+    /* JUnit 5 */
+
+    @org.junit.jupiter.api.Test
+    public void shouldProposeOneJ5() {
+        org.junit.jupiter.api.Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldProposeFiveHundredJ5() {
+        org.junit.jupiter.api.Assertions.assertEquals(500, service.remain(1500));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldProposeNullJ5() {
+        org.junit.jupiter.api.Assertions.assertNull(service.remain(2000));
     }
 }
